9900b0e6d64f05c81e453472c9853a79f06dbaa7
= <ismoilbek20050421@gmail.com>
2023-02-19 15:46:56 +0500

    constructor function

--- js/index.js
+++ js/index.js
@@ -1,34 +1,36 @@
-const isConfirmed = confirm("O'yinga tayyormisiz ?");
-let score = 0;
-
-if (isConfirmed) displayQuestion();
-else alert("Ok Fine Bro!");
-
-function displayQuestion() {
-  for (const question of questions) {
-    const questionMessage = `${question.question}
-A) ${question.answers.a}
-B) ${question.answers.b}
-C) ${question.answers.c}
-D) ${question.answers.d}`;
-
-    const result = prompt(questionMessage);
-    calculateScore(result, question.answer);
+function Quizz(isConfirmed) {
+  let score = 0;
+  this.isConfirmed = confirm(isConfirmed);
+  this.showResult = function () {
+    alert(`${score} of ${questions.length} correct answers`);
   }
+  this.calculateScore = function (answer, realAnswer) {
+    if (
+      answer == realAnswer
+    ) {
+      alert("Correct answer");
+      score++;
+    } else alert(`Wrong answer: Truthy value:  ${realAnswer}`);
+  }
+  this.displayQuestion = function () {
+    for (const question of questions) {
+      const questionMessage = `${question.question}
+  A) ${question.answers.a}
+  B) ${question.answers.b}
+  C) ${question.answers.c}
+  D) ${question.answers.d}`;
 
-  showResult();
+      const result = prompt(questionMessage).toLowerCase();
+      this.calculateScore(result, question.answer);
+    }
+
+    this.showResult();
+  }
 }
 
-function calculateScore(answer, realAnswer) {
-  if (
-    answer == realAnswer ||
-    answer.toUpperCase() === realAnswer.toUpperCase()
-  ) {
-    alert("Correct answer");
-    score++;
-  } else alert(`Wrong answer: Truthy value:  ${realAnswer}`);
-}
+const quizzApp = new Quizz("O'yinga tayyormisiz ?");
 
-function showResult() {
-  alert(`${score} of ${questions.length} correct answers`);
-}
+const isConfirmed = quizzApp.isConfirmed;
+
+if (isConfirmed) quizzApp.displayQuestion();
+else alert("Ok Fine Bro!");
